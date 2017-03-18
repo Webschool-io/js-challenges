b29d3ab8d0fcfa0d6678722f050176251bd7d22a
Suissa <jnascimento@gmail.com>
2017-03-17 21:21:13 -0300

    Testes

--- matrix-multiply/suissa/matrix.multiply.spec.js
+++ matrix-multiply/suissa/matrix.multiply.spec.js
@@ -16,55 +16,16 @@ const matrizResult = [
   [ 139, 154 ]
 ]
 
-const conteColunas = ( matriz ) => matriz[ 0 ].length
-const conteLinhas = ( matriz ) => 
-  matriz.filter( list => Array.isArray( list ) ).length
-
-const possoMultiplicar = ( m1, m2 ) => 
-  conteColunas( m1 ) === conteLinhas( m2 )
-
-const somandoTudo = ( a, b ) => a + b
-
-const multipliquePela = ( coluna ) => ( calculado, valorLinha, pos ) => {
-  calculado.push( valorLinha * coluna[ pos ] )
-  return calculado
-}
-
-const multipliqueLinhaColuna = ( linha, coluna ) => 
-  linha.reduce( multipliquePela( coluna ), [] )
-        .reduce( somandoTudo )
-
-const pegueLinha = ( pos, matriz ) => matriz[ pos ]
-const pegueColuna = ( pos, matriz ) => matriz.map( ( arr ) => arr[ pos ] )
-
-
-const matrizCalculada = ( [ m1, m2 ] = matrizes ) => 
-  m1.map( ( linha, pos ) => {
-    let contador = 0
-    let vezes = m1.length
-    const valores = []
-    while ( vezes ) {
-      valores.push( multipliqueLinhaColuna( pegueLinha( pos, m1 ), 
-                                            pegueColuna( contador, m2 ) ) )
-      --vezes
-      ++contador
-    }
-    if ( !vezes ) return valores
-  } )
-
-
-const resultadoFinal = matrizCalculada( [ matriz1, matriz2 ] )
+const Matriz = require('./matrix.multiply')
+const resultadoFinal = Matriz.matrizCalculada( [ matriz1, matriz2 ] )
 
 const testeUmValor = ( pos, [ m1, m2 ] = matrizes, matrizFinal ) => 
   assert.deepEqual( matrizFinal[pos][pos],
-                    multipliqueLinhaColuna( pegueLinha( pos, m1 ), 
-                                            pegueColuna( pos, m2 ) ) 
+                    Matriz.multipliqueLinhaColuna(Matriz.pegueLinha( pos, m1 ), 
+                                                  Matriz.pegueColuna( pos, m2 ) ) 
                   )
 
-const showError = ( expected, received ) => 
-  assert.deepEqual( expected, received )
-
-const testeTodosValores = ([ m1, m2 ] = matrizes, matrizFinal ) => 
+const testeTodosValores = ([ m1, m2 ] = matrizes, matrizFinal ) => {
   matrizFinal.map( (linha, posLinha) => 
     linha.map( (valor, posValor) => 
       ( matrizFinal[posLinha][posValor] === 
@@ -74,7 +35,7 @@ const testeTodosValores = ([ m1, m2 ] = matrizes, matrizFinal ) =>
                       resultadoFinal[posLinha][posValor] )
     )
   )
-
+}
 const specUmValor = {
   title: `\n Testar se o primeiro valor esta correto`,
   msgOK: `\t Primeiro valor - passou!`,
@@ -91,10 +52,8 @@ const specTodosValores = {
 }
 
 const showTitle = ( msg ) => { console.log(msg); return 1 }
-const runTest = ( spec ) => 
-  ( showTitle( spec.title ) && undefined === spec.result )
-    ? console.log( spec.msgOK )
-    : console.log( `\n\t FUUUUUUU!` )
+const showError = ( expected, received ) => 
+  assert.deepEqual( expected, received )
 
 const runTests = ( specs ) => 
   specs.map( ( spec ) => 
